4e67b3633a63978f101f24fee9eb674c7d8b2664
josevictor1 <josevictorpatos@gmail.com>
2020-04-14 20:49:40 -0300

    Testing result conversion with success and error

--- In-House Frameworks/Networking/NetworkingTests/Service/NetworkingServiceTests.swift
+++ In-House Frameworks/Networking/NetworkingTests/Service/NetworkingServiceTests.swift
@@ -12,7 +12,11 @@ import XCTest
 class NetworkingServiceTests: XCTestCase {
     
     // MARK: - Mocks
-    let mockError = NSError(domain: "", code: 0, userInfo: nil)
+    
+    let mockURL = URL(string: "www.test.com")!
+    var mockRequest: URLRequest  {
+        URLRequest(url: mockURL)
+    }
     
     // MARK: - Factories
     
@@ -20,39 +24,69 @@ class NetworkingServiceTests: XCTestCase {
         NetworkingService(requestProvider: URLRequestCreator())
     }
     
-    enum ResponseStatus {
-        case success
-        case redirection
-        case clientError
-        case serverError
-        case unknownError
+    enum ResponseStatusCode: Int {
+        case success = 200
+        case redirection = 300
+        case clientError = 400
+        case serverError = 500
+        case unknownError = 1000
     }
     
-    func makeHTTPURLResponse(with status: ResponseStatus) -> HTTPURLResponse {
-        let url = URL(string: "www.test.com")!
-        var statusCode: Int = 1000
-        switch status {
-        case .success:
-            statusCode = 200
-        case .redirection:
-            statusCode = 300
-        case .clientError:
-            statusCode = 400
-        case .serverError:
-            statusCode = 500
-        case .unknownError:
-            break
-        }
-        return HTTPURLResponse(url: url, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    func makeHTTPURLResponse(with statusCode: ResponseStatusCode) -> HTTPURLResponse {
+        HTTPURLResponse(url: mockURL, statusCode: statusCode.rawValue, httpVersion: nil, headerFields: nil)!
+    }
+    
+    func makeNSError(with statusCode: ResponseStatusCode) -> NSError {
+        NSError(domain: "", code: statusCode.rawValue, userInfo: nil)
     }
     
     // MARK: - Tests
     
+    func testConvertResponseToResultWithSuccess() {
+        let sut = makeSUT()
+        let httpURLResponseMock = makeHTTPURLResponse(with: .success)
+        let expectedResponse = NetworkingResponse(data: Data(), request: mockRequest, response: httpURLResponseMock)
+        var receivedResponse: NetworkingResponse?
+        
+        let result = sut.convertResponseToResult(Data(), mockRequest, httpURLResponseMock, nil)
+        
+        switch result {
+        case .success(let response):
+            receivedResponse = response
+        case .failure:
+            XCTFail()
+        }
+        
+        XCTAssertNotNil(receivedResponse)
+        XCTAssertEqual(receivedResponse, expectedResponse)
+    }
+    
+    func testConvertResponseToResultWithNetworkingError() {
+        let sut = makeSUT()
+        let mockError = makeNSError(with: .serverError)
+        let httpURLResponseMock = makeHTTPURLResponse(with: .serverError)
+        let responseMock = NetworkingResponse(data: Data(), request: mockRequest, response: httpURLResponseMock)
+        let expectedError: NetworkingError = .server(mockError, responseMock)
+        
+        var receivedError: NetworkingError?
+        
+        let result = sut.convertResponseToResult(nil, mockRequest, httpURLResponseMock, mockError)
+        
+        switch result {
+        case .success:
+            XCTFail()
+        case .failure(let error):
+            receivedError = error
+        }
+        
+        XCTAssertNotNil(receivedError)
+        XCTAssertEqual(receivedError, expectedError)
+    }
     
     func testConvertErrorToNetworkingErrorWithRedirection() {
         let sut = makeSUT()
-        let responseMock = NetworkingResponse(data: Data(),
-                                              response: makeHTTPURLResponse(with: .redirection))
+        let responseMock = NetworkingResponse(data: Data(), response: makeHTTPURLResponse(with: .redirection))
+        let mockError = makeNSError(with: .redirection)
         let expectedError: NetworkingError = .redirection(mockError, responseMock)
         
         let error = sut.convertErrorToNetworkingError(mockError, with: responseMock)
@@ -62,8 +96,8 @@ class NetworkingServiceTests: XCTestCase {
     
     func testConvertErrorToNetworkingErrorWithClientError() {
         let sut = makeSUT()
-        let responseMock = NetworkingResponse(data: Data(),
-                                              response: makeHTTPURLResponse(with: .clientError))
+        let responseMock = NetworkingResponse(data: Data(), response: makeHTTPURLResponse(with: .clientError))
+        let mockError = makeNSError(with: .clientError)
         let expectedError: NetworkingError = .client(mockError, responseMock)
         
         let error = sut.convertErrorToNetworkingError(mockError, with: responseMock)
@@ -73,8 +107,8 @@ class NetworkingServiceTests: XCTestCase {
     
     func testConvertErrorToNetworkingErrorWithServerError() {
         let sut = makeSUT()
-        let responseMock = NetworkingResponse(data: Data(),
-                                              response: makeHTTPURLResponse(with: .serverError))
+        let responseMock = NetworkingResponse(data: Data(), response: makeHTTPURLResponse(with: .serverError))
+        let mockError = makeNSError(with: .serverError)
         let expectedError: NetworkingError = .server(mockError, responseMock)
         
         let error = sut.convertErrorToNetworkingError(mockError, with: responseMock)
@@ -84,8 +118,8 @@ class NetworkingServiceTests: XCTestCase {
     
     func testConvertErrorToNetowrkingErrorWithUnknownError() {
         let sut = makeSUT()
-        let responseMock = NetworkingResponse(data: Data(),
-                                              response: makeHTTPURLResponse(with: .unknownError))
+        let responseMock = NetworkingResponse(data: Data(), response: makeHTTPURLResponse(with: .unknownError))
+        let mockError = makeNSError(with: .unknownError)
         
         let error = sut.convertErrorToNetworkingError(mockError, with: responseMock)
         
