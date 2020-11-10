4b6b60f8d1adf71fe824b85aa73bd349ad983ffb
josevictor1 <josevictorpatos@gmail.com>
2020-11-10 20:50:44 -0300

    Create GetUserNetworkingRequest and UserResponse to request user information.

---
+++ Features/GetFollowers/GetFollowers/Scenes/Get Followers/Model/Networking/GetUserNetworkingRequest.swift
@@ -0,0 +1,28 @@
+//
+//  GetUserNetworkingRequest.swift
+//  GetFollowers
+//
+//  Created by José Victor Pereira Costa on 10/11/20.
+//  Copyright © 2020 José Victor Pereira Costa. All rights reserved.
+//
+
+import Networking
+
+struct GetUserNetworkingRequest: Request {
+    
+    let username: String
+    
+    var method: HTTPMethod { .get }
+    
+    var scheme: URIScheme { .https }
+    
+    var host: String { "api.github.com" }
+    
+    var path: String { "/users/\(username)/followers" }
+    
+    var body: Encodable?
+    
+    var queryString: QueryString?
+    
+    var header: Header?
+}
---
+++ Features/GetFollowers/GetFollowers/Scenes/Get Followers/Model/Networking/UserResponse.swift
@@ -0,0 +1,79 @@
+//
+//  UserResponse.swift
+//  GetFollowers
+//
+//  Created by José Victor Pereira Costa on 10/11/20.
+//  Copyright © 2020 José Victor Pereira Costa. All rights reserved.
+//
+
+import Foundation
+
+struct UserResponse: Codable {
+    let login: String
+    let id: Int
+    let nodeID: String
+    let avatarURL: String
+    let gravatarID: String
+    let url: String
+    let htmlURL: String
+    let followersURL: String
+    let followingURL: String
+    let gistsURL: String
+    let starredURL: String
+    let subscriptionsURL: String
+    let organizationsURL: String
+    let reposURL: String
+    let eventsURL: String
+    let receivedEventsURL: String
+    let type: String
+    let siteAdmin: Bool
+    let name: String
+    let company: String
+    let blog: String
+    let location: String
+    let email: String
+    let hireable: Bool
+    let bio: String
+    let twitterUsername: String
+    let publicRepos: Int
+    let publicGists: Int
+    let followers: Int
+    let following: Int
+    let createdAt: Date
+    let updatedAt: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case url
+        case htmlURL = "html_url"
+        case followersURL = "followers_url"
+        case followingURL = "following_url"
+        case gistsURL = "gists_url"
+        case starredURL = "starred_url"
+        case subscriptionsURL = "subscriptions_url"
+        case organizationsURL = "organizations_url"
+        case reposURL = "repos_url"
+        case eventsURL = "events_url"
+        case receivedEventsURL = "received_events_url"
+        case type
+        case siteAdmin = "site_admin"
+        case name
+        case company
+        case blog
+        case location
+        case email
+        case hireable
+        case bio
+        case twitterUsername = "twitter_username"
+        case publicRepos = "public_repos"
+        case publicGists = "public_gists"
+        case followers
+        case following
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
