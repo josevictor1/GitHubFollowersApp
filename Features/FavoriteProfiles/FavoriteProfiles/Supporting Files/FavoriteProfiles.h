78e77a94cdcc3e20d81e027c307cc93bc39ce476
josevictor1 <josevictorpatos@gmail.com>
2021-05-30 15:02:24 -0300

    Creating and setup FavoriteProfiles module.

---
+++ Features/FavoriteProfiles/FavoriteProfiles/Supporting Files/FavoriteProfiles.h
@@ -0,0 +1,18 @@
+//
+//  FavoriteProfiles.h
+//  FavoriteProfiles
+//
+//  Created by José Victor Pereira Costa on 30/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FavoriteProfiles.
+FOUNDATION_EXPORT double FavoriteProfilesVersionNumber;
+
+//! Project version string for FavoriteProfiles.
+FOUNDATION_EXPORT const unsigned char FavoriteProfilesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FavoriteProfiles/PublicHeader.h>
+
+
